3a55f44696d2552e2e2bfe3c306d1c2e5377c959
jmechnich <joerg.mechnich@gmail.com>
2022-09-15 20:45:10 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+[![PyPI versio](https://img.shields.io/pypi/v/musicpal)](https://pypi.org/project/musicpal/)
+[![PyPi format](https://img.shields.io/pypi/format/musicpal)](https://pypi.org/project/musicpal/)
+[![PyPI license](https://img.shields.io/pypi/l/musicpal)](https://pypi.org/project/musicpal/)
+[![PyPi weekly downloads](https://img.shields.io/pypi/dw/musicpal)](https://pypi.org/project/musicpal/)
+
+musicpal
+==============
+
+Command line interface for remote controlling a Freecom MusicPal.
---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='musicpal',
+    author='Joerg Mechnich',
+    author_email='joerg.mechnich@gmail.com',
+    license='GNU GPLv3',
+    description='Command line interface for remote controlling a Freecom MusicPal',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/jmechnich/musicpal',
+    scripts=['musicpal'],
+    use_scm_version={"local_scheme": "no-local-version"},
+    setup_requires=['setuptools_scm'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.8',
+)
